d70a560035f03502b6132d2e0cd67fa90dae484f
nrinaudo <nicolas@nrinaudo.com>
2016-04-25 22:33:13 +0200

    Add missing joda time tests that somehow were not included in the previous commit.

---
+++ joda-time/src/test/scala/kantan/regex/joda/time/LocalDateDecoderTests.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2016 Nicolas Rinaudo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kantan.regex.joda.time
+
+import kantan.codecs.strings.joda.time.laws.discipline.arbitrary._
+import kantan.regex.laws.discipline.{GroupDecoderTests, MatchDecoderTests}
+import kantan.regex.laws.discipline.arbitrary._
+import org.joda.time.LocalDate
+import org.joda.time.format.DateTimeFormat
+import org.scalatest.FunSuite
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.typelevel.discipline.scalatest.Discipline
+
+class LocalDateDecoderTests extends FunSuite with GeneratorDrivenPropertyChecks with Discipline {
+  implicit val formatter = DateTimeFormat.mediumDate()
+
+  checkAll("GroupDecoder[LocalDate]", GroupDecoderTests[LocalDate].decoder[Int, Int])
+  checkAll("MatchDecoder[LocalDate]", MatchDecoderTests[LocalDate].decoder[Int, Int])
+}
---
+++ joda-time/src/test/scala/kantan/regex/joda/time/LocalTimeDecoderTests.scala
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2016 Nicolas Rinaudo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kantan.regex.joda.time
+
+import kantan.codecs.strings.joda.time.laws.discipline.arbitrary._
+import kantan.regex.laws.discipline.{GroupDecoderTests, MatchDecoderTests}
+import kantan.regex.laws.discipline.arbitrary._
+import org.joda.time.LocalTime
+import org.joda.time.format.DateTimeFormat
+import org.scalatest.FunSuite
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.typelevel.discipline.scalatest.Discipline
+
+class LocalTimeDecoderTests extends FunSuite with GeneratorDrivenPropertyChecks with Discipline {
+  implicit val formatter = DateTimeFormat.mediumTime()
+
+  checkAll("GroupDecoder[LocalTime]", GroupDecoderTests[LocalTime].decoder[Int, Int])
+  checkAll("MatchDecoder[LocalTime]", MatchDecoderTests[LocalTime].decoder[Int, Int])
+}
