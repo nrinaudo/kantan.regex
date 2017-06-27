2626ecc45994d654a93ddfce73829d230585d2a3
nrinaudo <nicolas@nrinaudo.com>
2017-06-27 22:44:21 +0200

    Upgrade to kantan.sbt 1.5.1

--- core/src/main/scala/kantan/regex/Errors.scala
+++ core/src/main/scala/kantan/regex/Errors.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Nicolas Rinaudo
+ * Copyright 2016 Nicolas Rinaudo
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 
 package kantan.regex
 
-import kantan.codecs._
+import kantan.codecs.error._
 
 /** Root class for all regular expression related errors. */
 sealed abstract class RegexError(msg: String) extends Error(msg)
