2999a8898d3e96a78920d7086d7886dcaadc713f
Nicolas Rinaudo <nicolas@nrinaudo.com>
2018-02-22 21:47:33 +0100

    [#71] Update to contextual 1.1.0

--- core/shared/src/main/scala/kantan/regex/literals/RegexLiteral.scala
+++ core/shared/src/main/scala/kantan/regex/literals/RegexLiteral.scala
@@ -18,26 +18,17 @@ package kantan.regex
 package literals
 
 import contextual._
-import java.util.regex.{Pattern, PatternSyntaxException}
+import java.util.regex.Pattern
 
-@SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
-object RegexLiteral extends Interpolator {
-  type Output = Pattern
-  def contextualize(interpolation: StaticInterpolation): Seq[ContextType] = {
-    interpolation.parts.foreach {
-      case lit @ Literal(_, _) ⇒
-        try Pattern.compile(interpolation.literals.head)
-        catch {
-          case p: PatternSyntaxException ⇒
-            // We take only the interesting part of the error message
-            val message = p.getMessage.split(" near").head
-            interpolation.error(lit, p.getIndex - 1, message)
-        }
-      case hole @ Hole(_, _) ⇒
-        interpolation.abort(hole, "substitution is not supported")
+object RegexLiteral extends Verifier[Pattern] {
+
+  override def check(string: String): Either[(Int, String), Pattern] =
+    // We could do a better job at analysing the error message and extracting the interesting bits, but:
+    // - this is not guaranteed to always work - error message might change format
+    // - it doesn't work with scala.js, and I'm not interested in this enough to have different error handling depending
+    //   on the target platform.
+    try { Right(Pattern.compile(string)) } catch {
+      case e: Exception ⇒ Left((0, e.getMessage))
     }
-    Nil
-  }
 
-  def evaluate(interpolation: RuntimeInterpolation): Pattern = Pattern.compile(interpolation.parts.mkString)
 }
